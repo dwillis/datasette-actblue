6194cf6f5d6451e3138523e95f605f25c15c7a48
Derek Willis <derek.willis@propublica.org>
2020-03-08 12:25:56 -0500

    loads into sqlite

--- actblue.py
+++ actblue.py
@@ -10,7 +10,8 @@ def actblue(filing, state):
     receipts = wget.download(receipts_url, bar=wget.bar_thermometer)
     spending_url = "https://pp-projects-static.s3.amazonaws.com/itemizer/sb_%s_%s.csv" % (filing, state.lower())
     spending = wget.download(spending_url, bar=wget.bar_thermometer)
-    subprocess.call(["csvs-to-sqlite", receipts, "actblue.db"])
+    subprocess.call(["csvs-to-sqlite", receipts, spending, "actblue.db"])
+    subprocess.call(["datasette actblue.db"])
 
 if __name__ == '__main__':
     actblue()
