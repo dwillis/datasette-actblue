b3353cb1d3c675c8aba28bb4c05552a085c30cb9
Derek Willis <derek.willis@propublica.org>
2020-03-08 12:14:24 -0500

    semi-working command

--- actblue.py
+++ actblue.py
@@ -1,7 +1,16 @@
 import wget
 import click
-
+import subprocess
 
 @click.command()
-@click.argument("state")
 @click.argument("filing")
+@click.argument("state")
+def actblue(filing, state):
+    receipts_url = "https://pp-projects-static.s3.amazonaws.com/itemizer/sa_%s_%s.csv" % (filing, state.lower())
+    receipts = wget.download(receipts_url, bar=wget.bar_thermometer)
+    spending_url = "https://pp-projects-static.s3.amazonaws.com/itemizer/sb_%s_%s.csv" % (filing, state.lower())
+    spending = wget.download(spending_url, bar=wget.bar_thermometer)
+    subprocess.call(["csvs-to-sqlite", receipts, "actblue.db"])
+
+if __name__ == '__main__':
+    actblue()
